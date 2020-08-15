2c22f0773f84b3b136587730801beee4ca27549c
S SAI SURYATEJA <31054270+sakurusurya2000@users.noreply.github.com>
2020-08-15 20:00:00 +0530

    Update Solution.java

--- 30 Days of Code/Day 26 - Nested Logic/Solution.java
+++ 30 Days of Code/Day 26 - Nested Logic/Solution.java
@@ -1,34 +1,32 @@
-// github.com/RodneyShag
-
-import java.util.Scanner;
-import java.time.LocalDate;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        /* Read and save input as LocalDates */
-        Scanner scan = new Scanner(System.in);
-        LocalDate returnDate = readDate(scan);
-        LocalDate expectDate = readDate(scan);
-        scan.close();
-        
-        /* Calculate fine */
-        int fine;
-        if (returnDate.isEqual(expectDate) || returnDate.isBefore(expectDate)) {
-            fine = 0;
-        } else if (returnDate.getMonth() == expectDate.getMonth() && returnDate.getYear() == expectDate.getYear()) {
-            fine = 15 * (returnDate.getDayOfMonth() - expectDate.getDayOfMonth());
-        } else if (returnDate.getYear() == expectDate.getYear()) {
-            fine = 500 * (returnDate.getMonthValue() - expectDate.getMonthValue());
-        } else {
-            fine = 10000;
-        }
-        System.out.println(fine);
-    }
-    
-    private static LocalDate readDate(Scanner scan) {
-        int day   = scan.nextInt();
-        int month = scan.nextInt();
-        int year  = scan.nextInt();
-        return LocalDate.of(year, month, day);
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int actualDay = sc.nextInt();
+		int actualMonth = sc.nextInt();
+		int actualYear = sc.nextInt();
+		int expectedDay = sc.nextInt();
+		int expectedMonth = sc.nextInt();
+		int expectedYear = sc.nextInt();
+
+		int fine;
+		if (actualYear > expectedYear) {
+			fine = 10000;
+		} else if (actualMonth > expectedMonth && (actualYear >= expectedYear)) {
+			fine = 500 * (actualMonth - expectedMonth);
+		} else if (actualDay > expectedDay && (actualMonth >= expectedMonth) && (actualYear >= expectedYear)) {
+			fine = 15 * (actualDay - expectedDay);
+		} else {
+			fine = 0;
+		}
+		System.out.println(fine);
+
+		sc.close();
+	}
 }
